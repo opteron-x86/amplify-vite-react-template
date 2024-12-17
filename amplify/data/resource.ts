2f9412b9d5ca2019f4949aad5be0862d265238d6
opteron-x86 <opteronx86@protonmail.com>
2024-12-17 11:52:43 -0500

    added per-user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -18,12 +18,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
     defaultAuthorizationMode: 'userPool',
-    },
   },
 });
 
